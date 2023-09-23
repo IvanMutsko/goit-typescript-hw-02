4bd9376a27d3b88f1a1b195ec8429bd6cb22eefe
Ivan Mutsko <imucko1996@gmail.com>
2023-09-23 15:23:41 +0300

    base types complite

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,17 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum WeekDay {
+  mon = "Monday",
+  tue = "Tuesday",
+  wed = "Wednesday",
+  thur = "Thursday",
+  fri = "Friday",
+  sat = "Saturday",
+  sun = "Sunday",
+}
+
+const isWeekend = (day: WeekDay): boolean => {
+  return day === WeekDay.sat || day === WeekDay.sun;
+};
